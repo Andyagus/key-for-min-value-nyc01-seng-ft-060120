425c0691eb368478c4943475ab6b9c582f4a8ccb
Andy Agus <redagus@gmail.com>
2020-05-25 17:33:21 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,46 +1,11 @@
-def key_for_min_value(name_hash)
-  lowest_key = nil 
-  lowest_value = nil 
-  hash.each do | key, value | 
-    if lowest_value==nil || value < lowest_value
-      lowest_value = value 
-      lowest_key = key
-    end
+def key_for_min_value(hash)
+ lowest_key  = nil
+ lowest_value = nil 
+ hash.each do |key, value|
+  if lowest_value == nil || value < lowest_value
+    lowest_value = value 
+    lowest_key = key
+   end
   end
+  lowest_key
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
